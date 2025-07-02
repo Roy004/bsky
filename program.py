7a3962f98fc453b23844b450bb2d3b32a3659981
Leonardo AC <leoamaya@enet.co.cu>
2025-07-02 16:25:06 -0400

    Tratando de obtener posts

--- program.py
+++ program.py
@@ -1,8 +1,24 @@
+
 from atproto import Client
+import json
 
-client=Client()
+def main():
+    client=Client()
 
-client.login('roinelperez.bsky.social','vagsZa5r')
+    try:
+        client.login('roinelperez.bsky.social','lhj5-tnbz-o7xq-36hn')
+        termino_busq='#cuba'
+        respuesta=client.app.bsky.feed.search_posts({'term':termino_busq, 'limit':25})
 
-post = client.send_post('Este es un post enviado con python!')
+        posts=respuesta.posts
 
+        for post in posts:
+            print(post)
+
+    except Exception as e:
+        print("Error: ", e)
+        return
+
+
+if __name__=="__main__":
+    main()
